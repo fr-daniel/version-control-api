d22f499eb8aa456d062248beb5f695e1cc56c7c4
Daniel Lima <fr.daniel.lima@gmail.com>
2020-08-02 23:01:52 -0300

    config cors

--- src/main/java/br/com/atlantico/versioncontrolapi/config/CorsFilter.java
+++ src/main/java/br/com/atlantico/versioncontrolapi/config/CorsFilter.java
@@ -1,8 +1,10 @@
 package br.com.atlantico.versioncontrolapi.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
+import org.springframework.core.env.Environment;
 
 import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
@@ -12,13 +14,16 @@ import java.io.IOException;
 @Configuration
 @Order(Ordered.HIGHEST_PRECEDENCE)
 public class CorsFilter implements Filter {
+    @Autowired
+    private Environment environment;
+
     @Override
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
         final HttpServletResponse response = (HttpServletResponse) res;
         HttpServletRequest request = (HttpServletRequest) req;
         String origin = request.getHeader("Origin");
         response.setHeader("Access-Control-Allow-Credentials", "true");
-        response.setHeader("Access-Control-Allow-Origin", origin != null && (origin.equals("http://localhost:8080") || origin.equals("https://sigaep.quixada.ufc.br")) ? origin : "");
+        response.setHeader("Access-Control-Allow-Origin", origin != null && (origin.equals(environment.getProperty("version-control.origin-permitida"))) ? origin : "");
         response.setHeader("Vary", "Origin");
         response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
         response.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Authorization, Content-Type, Accept, X-CSRF-TOKEN");
